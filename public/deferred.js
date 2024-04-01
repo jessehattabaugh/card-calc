7455c9b41abfb2bdb62ba3a5c7baf816ed5ddbe9
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2024-03-31 19:39:01 -0700

    make it slow

--- public/deferred.js
+++ public/deferred.js
@@ -3,3 +3,10 @@ import {CalcItem} from './CalcItem.js';
 
 customElements.define('calc-field',CalcField);
 customElements.define('calc-item',CalcItem);
+
+// populate main with a 1000 <calc-item> elements
+const main=document.querySelector('main');
+for(let i=0;i<1000;i++) {
+	const element=document.createElement('calc-item');
+	main.appendChild(element);
+}
